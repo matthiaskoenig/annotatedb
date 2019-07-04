943b908673fa3a0e7e7f39724e52d5f25dd9572b
Matthias Koenig <konigmatt@googlemail.com>
2019-07-04 18:54:16 +0200

    working on frontend table

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,46 +1,56 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/annotatedb.png')"
-          class="my-3"
-          contain
-          height="130"
-        ></v-img>
-      </v-flex>
+    <v-container>
+        <v-layout
+                text-xs-center
+                wrap
+        >
+            <v-flex xs12>
+                <v-flex mb-4>
+                    <v-img
+                            :src="require('../assets/annotatedb.png')"
+                            class="my-3"
+                            contain
+                            height="130"
+                    ></v-img>
+                </v-flex>
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to AnnotateDB<br />
-          <span class="font-weight-light">&nbsp; annotation mappings made easy</span>
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and questions, please open a
-          <a href="https://github.com/matthiaskoenig/annotatedb/issues" target="_blank">Github Issue</a>
-        </p>
-      </v-flex>
+                <v-flex mb-4>
+                    <h1 class="display-2 font-weight-bold mb-3">
+                        Welcome to AnnotateDB<br/>
+                        <span class="font-weight-light">&nbsp; annotation mappings made easy</span>
+                    </h1>
+                </v-flex>
+            </v-flex>
 
-    </v-layout>
-  </v-container>
+            <v-flex xs12>
+                <mappings-table></mappings-table>
+            </v-flex>
+
+            <v-flex xs12>
+            <p class="subheading font-weight-regular">
+                For help and questions, please open a
+                <a href="https://github.com/matthiaskoenig/annotatedb/issues" target="_blank">Github Issue</a>
+            </p>
+            </v-flex>
+
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-  import {lookup_icon} from "@/icons"
+    import {lookup_icon} from "@/icons"
+    import MappingsTable from "./MappingsTable";
 
-  export default {
-    data: () => ({
-    }),
-    methods: {
-      icon: function (key) {
-        return lookup_icon(key)
-      },
+    export default {
+        components: {MappingsTable},
+        data: () => ({}),
+        methods: {
+            icon: function (key) {
+                return lookup_icon(key)
+            },
+        }
+
     }
-
-  }
 </script>
 
 <style>
---
+++ frontend/src/components/MappingsTable.vue
@@ -0,0 +1,132 @@
+<template>
+    <v-card flat>
+        <v-toolbar id="heading-toolbar" dark>
+            <v-text-field
+                    v-model="search"
+                    append-icon="fa-search"
+                    label="Search"
+                    single-line
+                    hide-details
+                    :autofocus=true
+            />
+        </v-toolbar>
+            <v-data-table dark
+                    :headers="headers"
+                    :items="entries"
+                    :pagination.sync="pagination"
+                    :total-items="count"
+                    :loading="loading"
+                    :class="table_class"
+
+            >
+            <template slot="items" slot-scope="table">
+                <td>{{table.item.pk}}</td>
+                <td>{{table.item.qualifier}}</td>
+            </template>
+        </v-data-table>
+    </v-card>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: "MappingsTable",
+        components: {},
+        data () {
+            return {
+                search: '',
+                headers: [
+                    {text: 'pk', value: 'pk'},
+                    {text: 'qualifier', value: 'qualifier'},
+                ],
+                count: 0,
+                entries: [],
+                search:"",
+                loading: true,
+                pagination: {},
+                rowsPerPageItems: [5, 10, 20, 50, 100],
+                table_class: "elevation-1",
+            }
+        },
+        computed: {
+            resource_url() {
+                return this.$store.state.endpoints.api  + '/search/mappings/?format=json'
+            },
+            /**
+             * Create query url.
+             * @returns {string}
+             */
+            url() {
+                var url = this.resource_url
+                    + '&page='+ this.pagination.page
+                    + '&page_size='+ this.pagination.rowsPerPage
+                    + '&ordering='+ this.descending+ this.pagination.sortBy;
+                if (this.search){
+                    url += '&search_multi_match='+ this.search
+                }
+
+                return url
+            },
+            descending() {
+                return (this.pagination.descending ? "-" : "");
+            }
+        },
+        methods: {
+            icon(key) {
+                return lookup_icon(key)
+            },
+            searchUpdate (newValue) {
+                this.search = newValue
+            },
+            get_ids(array_of_obj) {
+                return array_of_obj.map(i => i.pk)
+            },
+
+            getData() {
+                var headers = {};
+                if (localStorage.getItem('token')) {
+                    var headers = {
+                        Authorization :  'Token ' + localStorage.getItem('token')
+                    }
+                }
+                axios.get(this.url, {headers: headers})
+                    .then(res => {
+                        this.entries = res.results;
+                        this.count = res.count;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+                    .finally(() => this.loading = false);
+            },
+        },
+        watch: {
+            pagination: {
+                handler () {
+                    this.getData()
+                },
+                deep: true
+            },
+            search: {
+                handler () {
+                    this.getData();
+                },
+                deep: true
+            },
+            url: {
+                handler () {
+                    this.getData();
+                },
+                deep: true
+            }
+        },
+        mounted () {
+            this.getData()
+        }
+    }
+</script>
+
+<style scoped></style>
+
+
